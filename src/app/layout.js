8e021e99aa229cd27519c7541672e1022f27dd81
fatiseddigh <seddighfati@gmail.com>
2025-08-28 21:31:58 +0400

    add HEADER

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
       <body className="min-h-screen">
-        <div>header</div>
+        <Header />
         <div className="container xl:max-w-screen-xl">{children}</div>
       </body>
     </html>
---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import NavLink from "./NavLink";
+
+const navLinks = [
+  { id: 1, children: "Home", path: "/" },
+  { id: 2, children: "Blogs", path: "/blogs" },
+];
+
+function Header() {
+  const user = false;
+
+  return (
+    <header
+      className="sticky top-0 z-50 backdrop-blur-lg
+                       bg-gradient-to-r from-primary-50/80 to-primary-100/80
+                       border-b-2 border-secondary-300 shadow-lg transition-all duration-300"
+    >
+      <nav className="container xl:max-w-screen-xl mx-auto flex flex-row items-center justify-between py-4 px-6 md:px-0">
+        {/* Logo */}
+        <div className="text-primary-700 font-extrabold text-lg md:text-2xl drop-shadow-md">
+          LOGO
+        </div>
+
+        {/* Navigation Links */}
+        <ul className="flex flex-row flex-wrap items-center gap-x-6 md:gap-x-8 text-secondary-400 font-medium">
+          {navLinks.map((navLink) => (
+            <li key={navLink.id}>
+              <NavLink
+                path={navLink.path}
+                className="relative text-secondary-400 hover:text-primary-700 transition-colors
+                           after:absolute after:left-0 after:-bottom-1 after:w-full after:h-0.5 after:bg-primary-500 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
+              >
+                {navLink.children}
+              </NavLink>
+            </li>
+          ))}
+          <li>
+            {user ? (
+              <NavLink
+                path="/profile"
+                className="ml-4 text-primary-500 font-semibold hover:text-primary-700 transition-colors"
+              >
+                Profile
+              </NavLink>
+            ) : (
+              <NavLink
+                path="/signin"
+                className="ml-4 text-primary-500 font-semibold hover:text-primary-700 transition-colors"
+              >
+                Login
+              </NavLink>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
