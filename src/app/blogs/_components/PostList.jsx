c8c45a999c86069f1e0683ee2cd2870532386c78
fatiseddigh <seddighfati@gmail.com>
2025-08-29 19:03:48 +0400

    add img

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,14 +1,31 @@
+import Image from "next/image";
+
 async function PostList() {
   await new Promise((res) => setTimeout(() => res(), 3000));
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
     data: { posts },
   } = await res.json();
-  return posts.length > 0
-    ? posts.map((post) => {
-        return <h1 key={post._id}>{post.title}</h1>;
-      })
-    : null;
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
+      {posts.map((post) => (
+        <div
+          key={post._id}
+          className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-300 p-2 rounded- 1g"
+        >
+          <div className="relative aspect-video overflow-hidden rounded-md">
+            <Image
+              src={post.coverImageUrl}
+              alt={post.title}
+              fill
+              className="object-cover object-center hover:scale-110 transition-all duration-300 ease-out"
+              quality={80}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  ) : null;
 }
 
 export default PostList;
