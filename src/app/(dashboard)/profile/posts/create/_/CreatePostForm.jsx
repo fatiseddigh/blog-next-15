d34b3adc6637864d4afda15b87e52d917df4ccb6
fatiseddigh <seddighfati@gmail.com>
2025-09-08 11:54:39 +0400

    add create post page api and submit

--- src/app/(dashboard)/profile/posts/create/_/CreatePostForm.jsx
+++ src/app/(dashboard)/profile/posts/create/_/CreatePostForm.jsx
@@ -3,8 +3,17 @@ import { useCategories } from "@/hooks/useCategories";
 import RHFSelect from "@/ui/RHFSelect";
 import RHFTextField from "@/ui/RHFTextField";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
+import useCreatePost from "./useCreatePost";
+import useEditPost from "./useEditPost";
+import { useRouter } from "next/navigation";
+import FileInput from "@/ui/FileInput";
+import ButtonIcon from "@/ui/ButtonIcon";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import SpinnerMini from "@/ui/SpinnerMini";
+import Image from "next/image";
 
 const schema = yup
   .object({
@@ -31,8 +40,39 @@ const schema = yup
   })
   .required();
 
-function CreatePostForm() {
+function CreatePostForm({ postToEdit = {} }) {
+  const { _id: editId } = postToEdit;
+  const isEditSession = Boolean(editId);
+  const {
+    title,
+    text,
+    slug,
+    briefText,
+    readingTime,
+    category,
+    coverImage,
+    coverImageUrl: prevCoverImageUrl,
+  } = postToEdit;
+
+  let editValues = {};
+  if (isEditSession) {
+    editValues = {
+      title,
+      text,
+      slug,
+      briefText,
+      readingTime,
+      category: category._id,
+      coverImage, // https://floan.ir/upload/folan.png => File !!!
+    };
+  }
+
   const { categories } = useCategories();
+  const [coverImageUrl, setCoverImageUrl] = useState(prevCoverImageUrl || null);
+  const { createPost, isCreating } = useCreatePost();
+  const { editPost, isEditing } = useEditPost();
+  const router = useRouter();
+
   const {
     control,
     reset,
@@ -43,10 +83,45 @@ function CreatePostForm() {
   } = useForm({
     mode: "onTouched",
     resolver: yupResolver(schema),
-    // defaultValues: editValues,
+    defaultValues: editValues,
   });
 
-  const onSubmit = (data) => {};
+  useEffect(() => {
+    if (prevCoverImageUrl) {
+      // convert preve link to file
+      async function fetchMyApi() {
+        const file = await imageUrlToFile(prevCoverImageUrl);
+        setValue("coverImage", file);
+      }
+      fetchMyApi();
+    }
+  }, [editId, prevCoverImageUrl, setValue]);
+
+  const onSubmit = (data) => {
+    const formData = new FormData();
+
+    for (const key in data) {
+      formData.append(key, data[key]);
+    }
+
+    if (isEditSession) {
+      editPost(
+        { id: editId, data: formData },
+        {
+          onSuccess: () => {
+            reset();
+            router.push("/profile/posts");
+          },
+        }
+      );
+    } else {
+      createPost(formData, {
+        onSuccess: () => {
+          router.push("/profile/posts");
+        },
+      });
+    }
+  };
 
   return (
     <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
@@ -104,16 +179,66 @@ function CreatePostForm() {
         options={categories}
         className="input-glass"
       />
+      <Controller
+        name="coverImage"
+        control={control}
+        rules={{ required: "Post cover image is required" }}
+        render={({ field: { value, onChange, ...rest } }) => {
+          return (
+            <FileInput
+              label="Choose your cover image"
+              name="coverImage"
+              isRequired
+              errors={errors}
+              {...rest}
+              value={value?.fileName}
+              onChange={(event) => {
+                const file = event.target.files[0];
+                // console.log(file);
+                onChange(file);
+                setCoverImageUrl(URL.createObjectURL(file));
+                event.target.value = null;
+              }}
+            />
+          );
+        }}
+      />
 
+      {coverImageUrl && (
+        <div className="relative aspect-video overflow-hidden rounded-lg">
+          <Image
+            fill
+            alt="cover-iamge"
+            src={coverImageUrl}
+            className="object-cover object-center"
+          />
+          <ButtonIcon
+            onClick={() => {
+              setCoverImageUrl(null);
+              setValue("coverImage", null);
+            }}
+            variant="red"
+            className="w-6 h-6 absolute left-4 top-4"
+          >
+            <XMarkIcon />
+          </ButtonIcon>
+        </div>
+      )}
       {/* Submit Button */}
-      <button
-        type="submit"
-        className="w-full py-3 px-4 rounded-xl font-medium
+      <div>
+        {isCreating || isEditing ? (
+          <SpinnerMini />
+        ) : (
+          <button
+            type="submit"
+            className="w-full py-3 px-4 rounded-xl font-medium
                    bg-gradient-to-r from-primary-600 via-primary-500 to-secondary-500
                    text-white shadow-md hover:shadow-xl transition"
-      >
-        Create Post
-      </button>
+          >
+            Create Post
+          </button>
+        )}
+      </div>
     </form>
   );
 }
